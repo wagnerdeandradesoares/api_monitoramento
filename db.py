afd10cc284c1858620c5def2da9fa4897b1cacfa
Wagner de Andrade <wagnergw770@gmail.com>
2025-11-02 15:55:42 -0300

    add banco

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import os
 
 # URL do MongoDB local ou remoto
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://wagnergw770_db_user:pBEa1npG18EbmhZf@monitoramento-cluster.bdozvv6.mongodb.net/?appName=monitoramento-cluster")
-DB_NAME = "monitoramento_bkp"
+DB_NAME = "monitoramento"
 
 client = MongoClient(MONGO_URL)
 db = client[DB_NAME]
